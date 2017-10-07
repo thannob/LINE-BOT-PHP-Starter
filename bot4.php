71b7231f8bcef46b14bd435848a0746bda10005e
thannob <iamaribarg@gmail.com>
2017-10-07 20:50:57 +0700

    fix code

--- bot4.php
+++ bot4.php
@@ -11,7 +11,7 @@ $arrHeader = array();
 $arrHeader[] = "Content-Type: application/json";
 $arrHeader[] = "Authorization: Bearer {$strAccessToken}";
  
-if($arrJson['events'][0]['message']['text'] == "text"){
+if($arrJson['events'][0]['message']['text'] == "ทดสอบ"){
   $arrPostData = array();
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
   $arrPostData['messages'][0]['type'] = "text";
