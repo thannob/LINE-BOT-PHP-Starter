ecb3e6c84efb0b297d5de6861c974029064719c0
thannob <iamaribarg@gmail.com>
2017-10-07 22:50:23 +0700

    edit code

--- bot3.php
+++ bot3.php
@@ -20,7 +20,7 @@ if($arrJson['events'][0]['message']['text'] == "คุณชื่ออะไ�
   $arrPostData = array();
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
   $arrPostData['messages'][0]['type'] = "template";
-  $arrPostData['messages'][0]['template']['type']= "Confitm";
+  $arrPostData['messages'][0]['template']['type']= "Confirm";
   $arrPostData['messages'][0]['template']['type']= "Are you sure?";
   $arrPostData['messages'][0]['template']['actions'][0]['type']= "message";
   $arrPostData['messages'][0]['template']['actions'][0]['label']= "Yes";
