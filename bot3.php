fde6b0e633a2cb0916bdf98f427eb2fc0ee43c8f
thannob <iamaribarg@gmail.com>
2017-10-07 22:41:51 +0700

    add bot3.php

---
+++ bot3.php
@@ -0,0 +1,43 @@
+﻿<?php
+ 
+$strAccessToken = "Oski+YFKV4aTJxTBVuR1KN+OvOBlNgTKi9YQI/lHz1fdnP5h7NPD0571e+9G1dwCQXmF08SKvDs+UU6tixRi8usk6MlF2IwNLygDxchhQf6lU+6UdnPMCzR4Wkrs51oaQc0AURQjWhU7hhOh7xNCmAdB04t89/1O/w1cDnyilFU=";
+ 
+$content = file_get_contents('php://input');
+$arrJson = json_decode($content, true);
+ 
+$strUrl = "https://api.line.me/v2/bot/message/reply";
+ 
+$arrHeader = array();
+$arrHeader[] = "Content-Type: application/json";
+$arrHeader[] = "Authorization: Bearer {$strAccessToken}";
+ 
+if($arrJson['events'][0]['message']['text'] == "คุณชื่ออะไร" ){
+  $arrPostData = array();
+  $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
+  $arrPostData['messages'][0]['type'] = "text";
+  $arrPostData['messages'][0]['text'] = "ผมชื่อ ธรรณพ อารีพรรค";
+}else{
+  $arrPostData = array();
+  $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
+  $arrPostData['messages'][0]['type'] = "template";
+  $arrPostData['messages'][0]['template'][0]['type']= "Confitm";
+  $arrPostData['messages'][0]['template'][0]['type']= "Are you sure?";
+  $arrPostData['messages'][0]['template'][0]['actions'][0]['type']= "message";
+  $arrPostData['messages'][0]['template'][0]['actions'][0]['label']= "Yes";
+  $arrPostData['messages'][0]['template'][0]['actions'][0]['text']= "yes";
+}
+ 
+ 
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL,$strUrl);
+curl_setopt($ch, CURLOPT_HEADER, false);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $arrHeader);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrPostData));
+curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+$result = curl_exec($ch);
+curl_close ($ch);
+ 
+echo "OK";
+?>
