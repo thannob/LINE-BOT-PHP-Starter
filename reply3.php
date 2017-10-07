829073a5c979172e7a612bfd6046c756fd980f93
thannob <iamaribarg@gmail.com>
2017-10-07 19:44:26 +0700

    fix code

--- reply3.php
+++ reply3.php
@@ -20,7 +20,7 @@ if($arrJson['events'][0]['message']['text'] == "text"){
   $arrPostData = array();
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
   $arrPostData['messages'][0]['type'] = "text";
-  $arrPostData['messages'][0]['text'] = "สวัสดี ID คุณคือ ".$arrJson['events'][0]['source']['userId'];
+  $arrPostData['messages'][0]['text'] = "สวัสดีครับ ID คุณคือ ".$arrJson['events'][0]['source']['userId'];
 }else if($arrJson['events'][0]['message']['text'] == "image"){
   $arrPostData = array();
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
