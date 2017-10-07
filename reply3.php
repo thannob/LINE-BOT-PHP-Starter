399d36b872e02cd50da963becd074f134c590317
thannob <iamaribarg@gmail.com>
2017-10-07 19:34:45 +0700

    add text code

--- reply3.php
+++ reply3.php
@@ -16,6 +16,11 @@ if($arrJson['events'][0]['message']['text'] == "text"){
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
   $arrPostData['messages'][0]['type'] = "text";
   $arrPostData['messages'][0]['text'] = "สวัสดีครับผมชื่อ ธรรณพ อารีพรรค";
+}elseif($arrJson['events'][0]['message']['text'] == "สวัสดี"){
+  $arrPostData = array();
+  $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
+  $arrPostData['messages'][0]['type'] = "text";
+  $arrPostData['messages'][0]['text'] = "สวัสดี ID คุณคือ ".$arrJson['events'][0]['source']['userId'];
 }else if($arrJson['events'][0]['message']['text'] == "image"){
   $arrPostData = array();
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
