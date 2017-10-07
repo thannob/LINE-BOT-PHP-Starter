69a050c72bb902248a2cc3f2cf8479e667f1e6cd
thannob <iamaribarg@gmail.com>
2017-10-07 17:03:50 +0700

    edit bot.php
    edit bot.php

--- bot.php
+++ bot.php
@@ -16,7 +16,7 @@ if (!is_null($events['events'])) {
 
 			// Get replyToken
 			$replyToken = $event['replyToken'];
-			echo $text . "\r\n";
+			echo $text . " TEXT\r\n";
 
 			if ($text = '1'){
 				$replyText = '1';
